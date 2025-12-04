52acb1372215cc44cf4151391d16dab29bc18215
bounthorn1 <wsite8999@gmail.com>
2025-12-04 10:31:02 +0700

    Initial commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_lkkcwxi",  // Replace with your EmailJS Service ID
+        "template_oh10swl",  // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "hRNCYNtecxusht7vO"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
